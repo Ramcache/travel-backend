c1c5e26e168e46810299b60a4a12efdee64268c3
Ramcache <ramaro@internet.ru>
2025-09-25 18:39:12 +0300

    fix

--- internal/models/order.go
+++ internal/models/order.go
@@ -1,13 +1,16 @@
 package models
 
-import "time"
+import (
+	"database/sql"
+	"time"
+)
 
 type Order struct {
-	ID        int       `json:"id"`
-	TripID    int       `json:"trip_id"`
-	UserName  string    `json:"user_name"`
-	UserPhone string    `json:"user_phone"`
-	Status    string    `json:"status"`
-	IsRead    bool      `json:"is_read"`
-	CreatedAt time.Time `json:"created_at"`
+	ID        int           `json:"id"`
+	TripID    sql.NullInt32 `json:"trip_id"`
+	UserName  string        `json:"user_name"`
+	UserPhone string        `json:"user_phone"`
+	Status    string        `json:"status"`
+	IsRead    bool          `json:"is_read"`
+	CreatedAt time.Time     `json:"created_at"`
 }
