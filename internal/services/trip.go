734ead34b7e9b7fdc9acff0167b2e674f58904bc
Ramcache <ramaro@internet.ru>
2025-10-06 12:32:51 +0300

    update

--- internal/services/trip.go
+++ internal/services/trip.go
@@ -359,7 +359,7 @@ func (s *TripService) BuyWithoutTrip(ctx context.Context, req models.BuyRequest)
 	//}
 
 	if s.telegram != nil {
-		link := fmt.Sprintf("%s/admin/orders", strings.TrimRight(s.frontendURL, "/"))
+		link := fmt.Sprintf("https://web95.tech/api/v1/admin/orders") // не забудь
 		if err := s.telegram.SendMessageWithButton(msg, "Открыть заказы", link); err != nil {
 			s.log.Errorw("Ошибка отправки заказа в Telegram", "order_id", order.ID, "err", err)
 			return err
